78e8f0ca1cfd72dfcf06655a61bd0d67a6a4157a
MihailAlexBuk <mihail.alexbuk@gmail.com>
2023-02-04 16:27:23 -0500

    implement views and layouts

--- core/Application.php
+++ core/Application.php
@@ -4,21 +4,27 @@ namespace app\core;
 
 use app\core\Router;
 use app\core\Request;
+use app\core\Response;
 
 class Application
 {
-
+    public static string $ROOT_DIR;
     public Router $router;
     public Request $request;
+    public Response $response;
+    public static Application $app;
 
-    public function __construct()
+    public function __construct($rootPath)
     {
+        self::$ROOT_DIR = $rootPath;
+        self::$app = $this;
         $this->request = new Request();
-        $this->router = new Router($this->request);
+        $this->response = new Response();
+        $this->router = new Router($this->request, $this->response);
     }
 
     public function run()
     {
-        $this->router->resolve();
+        echo $this->router->resolve();
     }
 }
--- core/Router.php
+++ core/Router.php
@@ -2,16 +2,16 @@
 
 namespace app\core;
 
-use app\core\Request;
-
 class Router
 {
-    protected array $routes = [];
     public Request $request;
+    public Response $response;
+    protected array $routes = [];
 
-    public function __construct(Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -19,16 +19,55 @@ class Router
         $this->routes['get'][$path] = $callback;
     }
 
+    public function post($path, $callback)
+    {
+        $this->routes['post'][$path] = $callback;
+    }
+
     public function resolve()
     {
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
-        if($callback === false)
+        if ($callback === false)
         {
-            echo 'Not found';
-            exit;
+            $this->response->setStatusCode(404);
+            return $this->renderView('_404');
         }
-        echo call_user_func($callback);
+
+        if (is_string($callback))
+        {
+            return $this->renderView($callback);
+        }
+
+        return call_user_func($callback);
     }
+
+    public function renderView($view)
+    {
+        $layoutsContent = $this->layoutsContent();
+        $viewContent = $this->renderOnlyView($view);
+        return str_replace('{{content}}', $viewContent, $layoutsContent);
+    }
+
+    public function renderContent($viewContent)
+    {
+        $layoutsContent = $this->layoutsContent();
+        return str_replace('{{content}}', $viewContent, $layoutsContent);
+    }
+
+    protected function layoutsContent()
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/layouts/main.php";
+        return ob_get_clean();
+    }
+
+    protected function renderOnlyView($view)
+    {
+        ob_start();
+        include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
+    }
+
 }
