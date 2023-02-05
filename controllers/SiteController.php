2ba1c8a6111314a0f22b62d885a12627519ae717
MihailAlexBuk <mihail.alexbuk@gmail.com>
2023-02-05 11:31:03 -0500

    Sanitize $_POST data

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,12 +4,14 @@ namespace app\controllers;
 
 use app\core\Application;
 use app\core\Controller;
+use app\core\Request;
 
 class SiteController extends Controller
 {
 
-    public function handleContact()
+    public function handleContact(Request $request)
     {
+        $body = $request->getBody();
         return 'post data';
     }
 
